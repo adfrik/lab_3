3954e8f20b9f0bd2e20d4ac0189e181b55a4af63
adfrik <bjryberg@gmail.com>
2019-09-20 13:38:22 -0600

    Searches a file for an expression

---
+++ lab3_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: Benjamin Ryberg Gonzalez and Jorge Ortiz
+# Date: 9/20/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+echo "Input a file name: "
+read fileName
+echo "Enter an expression: "
+read exp
+grep $exp $fileName >> "email_results.txt"
